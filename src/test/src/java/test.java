652e836ebfb7c965e5e3f3689e4120212c29a54d
TomChen001 <568418299@qq.com>
2017-07-12 22:42:47 +0800

    增加测试目录

---
+++ src/test/src/java/test.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+/**
+ * Created by ChenTao on 2017/7/12.
+ */
+public class test {
+
+    @Test
+    public void testDoWhile(){
+        int i =0;
+        do {
+           i++;
+        }while (i!=10);
+        System.out.println(i);
+    }
+}
