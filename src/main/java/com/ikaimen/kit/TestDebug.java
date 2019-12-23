1518b5d4fe6c5b530daa9fc782d9df06e2993b50
chentao <ikaimencxo@gmail.com>
2019-12-23 21:54:43 +0800

    add test

--- src/main/java/com/ikaimen/kit/TestDebug.java
+++ src/main/java/com/ikaimen/kit/TestDebug.java
@@ -1,17 +1,29 @@
 package com.ikaimen.kit;
 
+import java.util.ArrayList;
+
 public class TestDebug {
 
     public static void main(String[] args) {
 
-        int a = 10;
 
-        int b = 13;
+        ArrayList<Bear> bears = new ArrayList<>();
+        ArrayList<Bear> bears1 = new ArrayList<>();
 
+        Bear tom = new Bear("11", 2);
+        Bear jack = new Bear("jack", 1);
 
-        int c = a + b ;
+        bears.add(tom);
+        bears.add(jack);
 
-        System.out.println(c);
+        bears1.add(tom);
+        bears1.add(jack);
+
+        bears1.addAll(bears);
+        bears.clear();
+        char c = tom.getName().charAt(0);
+        System.out.println(Integer.valueOf(c));
+        System.out.println(bears.size());
 
     }
 
